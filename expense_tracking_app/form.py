2d6b185bc9973efa7415a20567cda254f452fd61
yosef <yosfemyayu@gmail.com>
2024-02-19 16:59:11 +0300

    user and profile form ready to use

--- expense_tracking_app/form.py
+++ expense_tracking_app/form.py
@@ -4,7 +4,6 @@ from .models import *
 from django.contrib.auth.models import User
 
 class Books_form(forms.ModelForm):
-   
     class Meta:
         model = Books
         exclude = ["modification_log"]
@@ -20,25 +19,24 @@ class Books_form(forms.ModelForm):
                 }
                 
 class User_form(forms.ModelForm):
-    username = forms.Charfield(requred = True,  widget=forms.TextInput(attrs={'class': 'form-control'})),
-    email = forms.EmailField(required = true ,widget=forms.TextInput(attrs={'class': 'form-control'}) ),
-    password1 = forms.EmailField(required = true ,widget=forms.TextInput(attrs={'class': 'form-control'}) ),
-    password2 = forms.PasswordField(required = true ,widget=forms.TextInput(attrs={'class': 'form-control'}) ),
-
+    username = forms.CharField(  widget=forms.TextInput(attrs={'requred' 'class': 'form-control'})),
+    email = forms.EmailField(required = True ,widget=forms.EmailInput(attrs={'class': 'form-control'}) ),
+    password1 = forms.CharField(required = True ,widget=forms.PasswordInput(attrs={'class': 'form-control'}) ),
+    password2 = forms.CharField(required = True ,widget=forms.PasswordInput(attrs={'class': 'form-control'}) ),
     # custom validation 
     class Meta:
         Model = User
         fields = ['username','email','password1','password2']
         def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords do not match")
-        return password2
+            password1 = self.cleaned_data.get('password1')
+            password2 = self.cleaned_data.get('password2')
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError("Passwords do not match")
+            return password2
 
-Class Profile_form()
+class Profile_form():
     avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    phone = forms.Charfield(requred = False,  widget=forms.TextInput(attrs={'class': 'form-control'})),
+    phone = forms.CharField(widget=forms.TextInput (attrs={'type':'tel','class': 'form-control'})),
     class Meta:
         Model = User
         fields = ['phone','avatar']
