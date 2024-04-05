fe4de574dfc48129670c18c649e9855b18336114
yosef <yosfemyau@gmail.com>
2024-04-05 23:12:48 +0300

    add profile view pading

--- expense_tracking_app/mailing.py
+++ expense_tracking_app/mailing.py
@@ -7,35 +7,23 @@ from email.mime.text import MIMEText
 def sendmailto(email, html_content,subject):
         message = MIMEMultipart("alternative")
         message["Subject"] = f"{subject}"
-        message["From"] = os.environ['dev_email']
+        print("============================================")
+        print(os.getenv('dev_email'))
+        print("============================================")
+
+        message["From"] = os.getenv('dev_email')
 
         message["To"] = email
         part = MIMEText(html_content, "html")
         message.attach(part)
         with smtplib.SMTP_SSL("smtp.gmail.com") as server:
-            server.login(os.environ['dev_email'] , os.environ['pass_key'])
-            server.sendmail(os.environ['dev_email'],
+            server.login(os.getenv('dev_email') , os.getenv('pass_key'))
+            server.sendmail(os.getenv('dev_email'),
                             email, message.as_string())
             server.quit()
-def generatePassword(self, email,name,username):
-        passKey = ""
-        # password buffer
-        buffer = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz@#$%&*"
-
-        # generate 6 digit (random password)
-        for i in range(6):
-            passKey += random.choice(buffer)
-
-        if isreset:
-            self.sendCode(
-             email=email, username=username, passKey=passKey,subject = "Password Reset",content="AS per your request your password has been reset")
-        else:
-            self.sendCode(
-             email=email, username=username, passKey=passKey,subject = "Access Granted",content="Now you get an access to POS monitoring tool")
-        return passKey
 
 def deliver_password_reset(subject,name,email,username,password):
-    html_content = f""" 
+    html_content = f"""
                 <!DOCTYPE html>
                 <html lang="en">
                 <head>
@@ -44,7 +32,7 @@ def deliver_password_reset(subject,name,email,username,password):
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Document</title>
                 </head>
-                
+
                 <body style="font-family: 'Times New Roman', Times, serif;">
                 <table cellpadding="0" cellspacing="0" border="0" align="center" style="background-color: rgb(217, 216, 216); margin: 4% 10%; border-bottom: outset 6px rgb(255, 166, 0); border-radius: 0 30px;">
                     <tr>
@@ -67,6 +55,5 @@ def deliver_password_reset(subject,name,email,username,password):
                 </table>
                 </body>
                 </html>
-
                 """
     sendmailto(email, html_content,subject)
